19932f2be0a3fc7df0650063ab6c24647b3759fe
Steven Wang <sw1190@gmail.com>
2018-04-06 17:06:53 -0700

    fix extra code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,13 @@
 class SessionsController < ApplicationController
 
   def create
-    begin
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-      session[:user_id] = @user.id
-      @hash = request.env['omniauth.auth'].to_json
-      # flash[:success] = request.env['omniauth.auth']
-    rescue
-      flash[:warning] = "There was an error while trying to authenticate you..."
-    end
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    session[:user_id] = @user.id
     redirect_to root_path
-    # render 'home/index'
   end
 
   def destroy
-    if current_user
-      session.delete(:user_id)
-    end
+    session.delete(:user_id)
     redirect_to root_path
   end
 end
