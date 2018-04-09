6731ef08424be50434109343cb934dc8c6015dd7
Steven Wang <sw1190@gmail.com>
2018-04-09 16:02:00 -0700

    add logic to prevent duplicate messages being processed when multiple users on the same stream

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,13 +6,17 @@ class Message < ApplicationRecord
 
   def self.add(messages)
     messages.each do |message|
-      current_message = find_or_create_by(message_id: message['id'])
-      current_message.author = message['authorDetails']['displayName']
-      current_message.author_image = message['authorDetails']['profileImageUrl']
-      current_message.body = message['snippet']['displayMessage']
-      current_message.chat_id = message['snippet']['liveChatId']
-      current_message.save!
-      current_message
+      current_message = find_by(message_id: message['id'])
+      unless current_message
+        current_message = Message.new
+        current_message.message_id = message['id']
+        current_message.author = message['authorDetails']['displayName']
+        current_message.author_image = message['authorDetails']['profileImageUrl']
+        current_message.body = message['snippet']['displayMessage']
+        current_message.chat_id = message['snippet']['liveChatId']
+        current_message.save!
+        current_message
+      end
     end
   end
 end
